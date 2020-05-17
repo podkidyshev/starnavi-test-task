3f926cc697abca1142abccbce237d18087bba8ee
Ivan Podkidyshev <raashicat@gmail.com>
2020-05-17 18:47:58 +0400

    PostSerializer.author -> User.username. setting likes_count on Post after creation

--- src/api/post/serializers.py
+++ src/api/post/serializers.py
@@ -1,15 +1,21 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
 
 from starnavi.post.models import Post
 
 
 class PostSerializer(ModelSerializer):
+    author = SlugRelatedField(slug_field='username', read_only=True)
+
     class Meta:
         model = Post
         fields = ['id', 'author', 'title', 'description', 'date_created', 'likes_count']
-        read_only_fields = ['author', 'likes_count']
 
     def validate(self, attrs):
         attrs = super().validate(attrs)
         attrs['author'] = self.context['request'].user
         return attrs
+
+    def create(self, validated_data):
+        post = super().create(validated_data)
+        post._likes_count = 0
+        return post
